0f3c0e9cc370f7f05e00d2d29c6bc98eccf603bd
Asskicker25 <sp68860@gmail.com>
2023-12-12 00:58:23 -0500

    Third commit

--- Graphics_Final_Exam/Graphics_Final_Exam/src/Environment/Terrain/Terrain.cpp
+++ Graphics_Final_Exam/Graphics_Final_Exam/src/Environment/Terrain/Terrain.cpp
@@ -17,6 +17,7 @@ void Terrain::Update(float deltaTime)
 
 void Terrain::AddToRendererAndPhysics(Renderer* renderer, Shader* shader, PhysicsEngine* physicsEngine)
 {
+	model->LoadModel()
 }
 
 void Terrain::RemoveFromRendererAndPhysics(Renderer* renderer, PhysicsEngine* physicsEngine)
